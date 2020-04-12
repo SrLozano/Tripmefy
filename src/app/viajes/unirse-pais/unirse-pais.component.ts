9c369dc0218920299da60bc9ef5a3d7b68e6c469
MiaML2 <100383520@alumnos.uc3m.es>
2020-04-12 20:48:22 +0200

    Front-end unirse-ciudad desktop

--- src/app/viajes/unirse-pais/unirse-pais.component.ts
+++ src/app/viajes/unirse-pais/unirse-pais.component.ts
@@ -7,6 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UnirsePaisComponent implements OnInit {
 
+  title = 'Europa';
+  //paises = ['España', 'Francia', 'Italia', 'Bélgica', 'Rumanía', 'Eslovaquia', 'Chipre', 'Malta', 'Bielorrusia', 'Ciudad del Vaticano', 'Bulgaria', 'Polonia', 'Suiza', 'Alemania'];
+  europa = [
+    {
+      "pais": "España", 
+      "imagen": "sp.png"
+    },
+    {
+      "pais": "Francia",
+      "imagen": "fr.png"
+    },
+    {
+      "pais": "Hungría",
+      "imagen": "hu.png"
+    },
+    {
+      "pais": "Rumanía",
+      "imagen": "zw.png"
+    },
+    {
+      "pais": "Eslovaquia",
+      "imagen": "zw.png"
+    },
+    {
+      "pais": "Chipre",
+      "imagen": "zw.png"
+    },
+
+  ] 
+  mypais= this.europa[0];
   constructor() { }
 
   ngOnInit(): void {
