b323745cc1c3cec6fce02ed800272df2b464d474
SarayGrj <saraygrj@gmail.com>
2020-04-16 10:55:48 +0200

    scroll-top and politica finished

--- src/app/privacidad/privacidad.component.ts
+++ src/app/privacidad/privacidad.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, HostListener } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-privacidad',
@@ -6,28 +6,12 @@ import { Component, OnInit, HostListener } from '@angular/core';
   styleUrls: ['./privacidad.component.scss']
 })
 export class PrivacidadComponent implements OnInit {
-  navIsFixed: boolean;
+  
   constructor() { }
 
-  @HostListener("window:scroll", [])
-  onWindowScroll() {
-      if (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop > 100) {
-          this.navIsFixed = true;
-      } else if (this.navIsFixed && window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop < 10) { this.navIsFixed = false; } } scrollToTop() { (function smoothscroll() { var currentScroll = document.documentElement.scrollTop || document.body.scrollTop; if (currentScroll > 0) {
-              window.requestAnimationFrame(smoothscroll);
-              window.scrollTo(0, currentScroll - (currentScroll / 5));
-          }
-      })();
-  }
 
   ngOnInit(): void {
   }
 
-  onClick(){
-    console.log("Botón pulsado");
-    if (window && window.pageYOffset) {
-      window.scroll(0, 0);
-    }
-  }
 
 }
--- src/app/shared/scroll-top/scroll-top.component.ts
+++ src/app/shared/scroll-top/scroll-top.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener  } from '@angular/core';
 
 @Component({
   selector: 'app-scroll-top',
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./scroll-top.component.scss']
 })
 export class ScrollTopComponent implements OnInit {
-
+  navIsFixed: boolean;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+      if (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop > 100) {
+          this.navIsFixed = true;
+      } else if (this.navIsFixed && window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop < 10) { this.navIsFixed = false; } } scrollToTop() { (function smoothscroll() { var currentScroll = document.documentElement.scrollTop || document.body.scrollTop; if (currentScroll > 0) {
+              window.requestAnimationFrame(smoothscroll);
+              window.scrollTo(0, currentScroll - (currentScroll / 5));
+          }
+      })();
+  }
 }
