daf4671b713eeb71c18cfd80f0b07468e3e39973
Mario Lozano Cortés <58252542+SrLozano@users.noreply.github.com>
2020-04-26 19:53:28 +0200

    Buttons added in the header component and pop-ups

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,7 +1,13 @@
-import { Component, OnInit, Input} from '@angular/core';
+import { Component, OnInit, Input, Inject} from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { ActivatedRoute }  from '@angular/router';
 import { Router } from '@angular/router'; //Para redirigir a una página
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+
+export interface DialogData {
+  animal: string;
+  name: string;
+}
 
 @Component({
   selector: 'app-header',
@@ -12,7 +18,7 @@ import { Router } from '@angular/router'; //Para redirigir a una página
 export class HeaderComponent implements OnInit {
 @Input() public titulo:string;
   constructor(private authSvc : AuthService,
-             private _route:ActivatedRoute, private route: Router) {
+             private _route:ActivatedRoute, private route: Router, public dialog: MatDialog) {
     this.titulo = "";
    }
 
@@ -53,6 +59,56 @@ export class HeaderComponent implements OnInit {
   }
 
   onLogout():void{
-    this.authSvc.logOut();
+    
+    //ABRIR POP-UP AL PULSAR LOGOUT
+    const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
+      width: '250px',
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
   }
+
+  onNotification():void{
+    const dialogRef = this.dialog.open(DialogOverviewNNotificationDialog, {
+      width: '250px',
+    });
+  }
+}
+
+
+@Component({
+  selector: 'dialog-overview-notification',
+  templateUrl: 'dialog-overview-notification.html',
+})
+export class DialogOverviewNNotificationDialog { //FUNCIONES POP-UP NOTIFICATION
+
+  constructor(
+    public dialogRef: MatDialogRef<DialogOverviewNNotificationDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData, private authSvc : AuthService) {}
+
+}
+
+
+@Component({
+  selector: 'dialog-overview-example-dialog',
+  templateUrl: 'dialog-overview-example-dialog.html',
+})
+export class DialogOverviewExampleDialog { //FUNCIONES POP-UP LOGUT
+
+  constructor(
+    public dialogRef: MatDialogRef<DialogOverviewExampleDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData, private authSvc : AuthService) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  onYesClick(): void {
+    this.authSvc.logOut();
+    var pagInicio = window.location.origin + '/page1'; 
+    window.location.assign(pagInicio);
+  }
+
 }
