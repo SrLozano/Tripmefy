227c98636722dc0826671af9730248c160540790
MiaML2 <100383520@alumnos.uc3m.es>
2020-04-27 17:53:50 +0200

    logros css changed

--- src/app/logros/logros.component.ts
+++ src/app/logros/logros.component.ts
@@ -12,47 +12,56 @@ export class LogrosComponent implements OnInit {
     {
       'nombre': 'Súper viajero',
       'descripcion': 'Has estado en todos los continentes',
-      'img': 'logros/logro1.png'
+      'img': 'logros/logro1.png',
+      'activado': 'no'
     },
     {
       'nombre': 'Viajero amigo',
       'descripcion': 'Has hecho un viaje con más de seis personas',
-      'img': 'logros/logro2.png'
+      'img': 'logros/logro2.png',
+      'activado': 'no'
     },
     {
       'nombre': 'Europa',
       'descripcion': 'Has estado en Europa',
-      'img': 'logros/logro3.png'
+      'img': 'logros/logro3.png',
+      'activado': 'no'
     },
     {
     'nombre': 'Asia',
       'descripcion': 'Has estado en Asia',
-      'img': 'logros/logro4.png'
+      'img': 'logros/logro4.png',
+      'activado': 'no'
     },
     {
     'nombre': 'África',
       'descripcion': 'Has estado en África',
-      'img': 'logros/logro5.png'
+      'img': 'logros/logro5.png',
+      'activado': 'no'
     },
     {
     'nombre': 'Oceanía',
       'descripcion': 'Has estado en Oceanía',
-      'img': 'logros/logro6.png'
+      'img': 'logros/logro6.png',
+      'activado': 'no'
     },
     {
     'nombre': 'América del Norte',
       'descripcion': 'Has estado en América del Norte',
-      'img': 'logros/logro7.png'
+      'img': 'logros/logro7.png',
+      'activado': 'no'
     },
     {
     'nombre': 'América del Sur',
       'descripcion': 'Has estado en América del Sur',
-      'img': 'logros/logro8.png'
+      'img': 'logros/logro8.png',
+      'activado': 'no'
     },
     {
     'nombre': 'América Central',
       'descripcion': 'Has estado en América Central',
-      'img': 'logros/logro9.png'
+      'img': 'logros/logro9.png',
+      'activado': 'no'
     },
   ]
   ngOnInit(): void {
