73c8d26c778e1d5a4a7aa60a48b63bdfae39aa59
SarayGrj <saraygrj@gmail.com>
2020-04-27 12:59:32 +0200

    logros

--- src/app/logros/logros.component.ts
+++ src/app/logros/logros.component.ts
@@ -12,48 +12,47 @@ export class LogrosComponent implements OnInit {
     {
       'nombre': 'Súper viajero',
       'descripcion': 'Has estado en todos los continentes',
-      'img2': 'logros/log1.png',
-      'img': 'banderas/hu.png'
+      'img': 'logros/logro1.png'
     },
     {
       'nombre': 'Viajero amigo',
       'descripcion': 'Has hecho un viaje con más de seis personas',
-      'img': 'logros/log2.png'
+      'img': 'logros/logro2.png'
     },
     {
       'nombre': 'Europa',
       'descripcion': 'Has estado en Europa',
-      'img': 'logros/log3.png'
+      'img': 'logros/logro3.png'
     },
     {
     'nombre': 'Asia',
       'descripcion': 'Has estado en Asia',
-      'img': 'logros/log4.png'
+      'img': 'logros/logro4.png'
     },
     {
     'nombre': 'África',
       'descripcion': 'Has estado en África',
-      'img': 'logros/log5.png'
+      'img': 'logros/logro5.png'
     },
     {
     'nombre': 'Oceanía',
       'descripcion': 'Has estado en Oceanía',
-      'img': 'logros/log6.png'
+      'img': 'logros/logro6.png'
     },
     {
     'nombre': 'América del Norte',
       'descripcion': 'Has estado en América del Norte',
-      'img': 'logros/log7.png'
+      'img': 'logros/logro7.png'
     },
     {
     'nombre': 'América del Sur',
       'descripcion': 'Has estado en América del Sur',
-      'img': 'logros/log8.png'
+      'img': 'logros/logro8.png'
     },
     {
     'nombre': 'América Central',
       'descripcion': 'Has estado en América Central',
-      'img': 'logros/log9.png'
+      'img': 'logros/logro9.png'
     },
   ]
   ngOnInit(): void {
