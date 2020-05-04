816cf9f8a7b66ac445089ff3165463b50442238b
SarayGrj <saraygrj@gmail.com>
2020-05-04 15:25:49 +0200

    refresh works

--- dist/Tripmefy/polyfills-es2015.js
+++ dist/Tripmefy/polyfills-es2015.js
@@ -3033,7 +3033,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\maria\Desktop\mia\mis-carpetas\work\uni\SextoCuatri\DSI\Tripmefy\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! /Users/eugeniogarciadelarosa/github/saraygrjim/Tripmefy/src/polyfills.ts */"./src/polyfills.ts");
 
 
 /***/ })
