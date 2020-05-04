fc942192e47837ec78e0421c8d5ec14513c56558
SrLozano <100383511@alumnos.uc3m.es>
2020-05-04 13:55:48 +0200

    Fail while hosting

--- dist/Tripmefy/polyfills-es2015.js
+++ dist/Tripmefy/polyfills-es2015.js
@@ -3033,7 +3033,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/eugeniogarciadelarosa/github/saraygrjim/Tripmefy/src/polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! /Users/mariolozanocortes/Universidad_Local/Tripmefy/src/polyfills.ts */"./src/polyfills.ts");
 
 
 /***/ })
